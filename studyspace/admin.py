6ca2490c50834dd922c4a1ae84d8eb4f2bf0e992
mangabear4dn <luize.blake@gmail.com>
2022-03-22 14:28:31 +0000

    Add admin registration for both models with adjustments

--- studyspace/admin.py
+++ studyspace/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Reservation, Space
 
-# Register your models here.
+
+@admin.register(Space)
+class SpaceAdmin(admin.ModelAdmin):
+    """AdminClass for Spaces"""
+    list_filter = ['type', 'max_capacity']
+    list_display = ['name', 'max_capacity', 'type', 'available_tech']
+    search_fields = ['name']
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    """AdminClass for Reservations"""
+    list_filter = ['start', 'space__type', 'snacks_discount']
+    list_display = ['start', 'username', 'space', 'snacks_discount']
+    search_fields = ['username']
